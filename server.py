aeb0516d085c74101dacc7d4c362b4b02df9b899
Omar Faruk <munna93_bd@yahoo.com>
2015-05-02 03:03:39 +0600

    python 3.4 conflicts are fixed

--- server.py
+++ server.py
@@ -6,17 +6,17 @@ host = str([(tmp.connect(('8.8.8.8', 80)), tmp.getsockname()[0], tmp.close()) fo
 port = 12345
 s.bind((host, port))
 s.listen(5)
-print host+" is listening on port ",port
+print (host+" is listening on port ",port)
 while True:
     c, addr = s.accept()
     l = c.recv(48)
     filename=l
-    print filename+" Receiving..."
+    print (filename+" Receiving...")
     f = open(filename,'wb')
     while (l):
         l = c.recv(1024)
         f.write(l)
     f.close()
-    print "Done Receiving"
+    print ("Done Receiving")
 c.close()
 s.close()
